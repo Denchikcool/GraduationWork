56c5f5908fe5ce4720612eb0b5baf7a0e9337b5b
Denchikcool <kuzmenok.den@yandex.ru>
2025-04-24 17:46:31 +0700

    Решил поменять Input System, чтоб ещё можно было играть на геймпаде. Переделка Input Controller

---
+++ Action Platformer/Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private Vector2 _movementInput;
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        _movementInput = context.ReadValue<Vector2>();
+        Debug.Log(_movementInput);
+    }
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log("Jump button pushed down now");
+        }
+        if (context.performed)
+        {
+            Debug.Log("Jump button is being held down");
+        }
+        if (context.canceled)
+        {
+            Debug.Log("Jump button has been released");
+        }
+    }
+}
